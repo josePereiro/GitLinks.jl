38e0cc89cca60e9e0b4119407629ba15fefc81b8
josePereiro <jose.pereiro.lab@gmail.com>
2021-11-29 06:44:13 -0500

    Add a stage flag (to control upload)

--- src/Server/stage.jl
+++ src/Server/stage.jl
@@ -23,10 +23,11 @@ end
 
 # TODO: Test this
 """
-    stage(gl::GitLink, files::Vector{String}; root::String = "", tout = 60.0)
+    stage(gl::GitLink, files::Vector{String}; root::String = "", tout = 60.0, flag = true)
 
 Stage the `files` (by copying them to the GitLink stage folder).
 The GitLink Server will upload them in its next iter.
+To prevent this set the acknowledge (`flag`) to false.
 This method will sleep till (or timeout `tout`) the GitLink lock is free (which must by must of the time, but...).
 If a `root` (as a kwarg) is provided, the tree structure relative to it will be respected.
 Ex: if root = "A" and a file path is "A/B/C.txt" it will be staged as "gl-stage/B/C.txt".
@@ -34,7 +35,7 @@ Otherwise (root = ""), the file will be copied in the repo's stage "gl-stage/C.t
 Returns `true` if the action was succeful.
 """
 function stage(gl::GitLink, files::Vector{String}; 
-        root::String = "", tout = 60.0
+        root::String = "", tout = 60.0, flag = true
     )
 
     ok_flag = false
@@ -57,25 +58,26 @@ function stage(gl::GitLink, files::Vector{String};
                 cp(src_file, dest_file; force = true)
             end
         end
-        _set_stage_token(gl)
+        flag && _set_stage_token(gl)
         ok_flag = true
     end
     return ok_flag
 end
 
 """
-    stage(upfun::Function, gl::GitLink; tout = 60.0))
+    stage(upfun::Function, gl::GitLink; tout = 60.0, flag = true)
 
 Allow to modify the stage directory of the GitLink.
 The function `upfun(stage_dir)` will be executed and it should copy/create/modify
 the files into `stage_dir`.
 It is recommended that `upfun` not to be an expensive function.
 The GitLink Server will upload the staged files in its next iter.
+To prevent this set the acknowledge (`flag`) to false.
 This method will sleep till (or timeout `tout`) the GitLink lock is free (which must by must of the time, but...).
 Returns `true` if the action was succeful.
 """
 function stage(upfun::Function, gl::GitLink; 
-        tout = 60.0
+        tout = 60.0, flag = true
     )
 
     ok_flag = false
@@ -84,7 +86,7 @@ function stage(upfun::Function, gl::GitLink;
 
     lock(gl; tout) do
         upfun(sdir)
-        _set_stage_token(gl)
+        flag && _set_stage_token(gl)
         ok_flag = true
     end
 
